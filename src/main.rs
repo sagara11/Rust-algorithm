0d586416a00ee536142566079bc025ba94a6ba99
Thong Nguyen <thong.nguyen@savvycomsoftware.com>
2023-03-02 18:35:45 +0700

    Dijkstra-alogrithm

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,102 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
-#[derive(Eq, Hash, PartialEq, Debug)]
-pub struct Person {
-    name: String,
-    value: u128,
-}
-
-pub fn have_proper_value(user: &Person) -> bool {
-    if user.value == 34 {
-        return true;
+pub fn initialize_costs<'a>(
+    costs: &mut HashMap<&'a str, f64>,
+    graph: &HashMap<&'a str, HashMap<&'a str, f64>>,
+) {
+    let inf = f64::INFINITY;
+    for (k, _) in graph {
+        costs.insert(k, inf);
     }
 
-    false
+    let start_map = graph.get("Start").unwrap();
+
+    for (k, v) in start_map {
+        costs.entry(k).and_modify(|e| *e = *v).or_default();
+    }
 }
 
-pub fn breadth_first_search(
-    graph: HashMap<&Person, Vec<&Person>>,
-    initial_value: &Person,
-) -> String {
-    let mut result = String::new();
-    let mut queue: VecDeque<&Person> = VecDeque::new();
-    let neighbors_node = graph.get(initial_value).unwrap();
-    for e in neighbors_node {
-        queue.push_front(e);
+pub fn initialize_parent<'a>(
+    parents: &mut HashMap<&'a str, &str>,
+    graph: &HashMap<&'a str, HashMap<&'a str, f64>>,
+) {
+    for (k, _) in graph {
+        parents.insert(k, "none");
     }
-    let mut searched: HashMap<&Person, bool> = HashMap::new();
+}
 
-    while queue.len() > 0 {
-        let node = queue.pop_back().unwrap();
-        if searched.get(node) == None {
-            if have_proper_value(node) {
-                result.push_str(node.name.as_str());
-                break;
-            } else {
-                let neighbors_node = graph.get(node).unwrap();
-                for e in neighbors_node {
-                    queue.push_front(e);
-                }
-                searched.insert(node, true);
+pub fn find_lowest_cost_node<'a>(
+    costs: &HashMap<&'a str, f64>,
+    searched: &HashMap<&str, bool>,
+) -> Option<&'a str> {
+    let inf = f64::INFINITY;
+    let mut lowest_cost = inf;
+    let mut lowest_cost_node: Option<&str> = None;
+
+    for (k, v) in costs {
+        if !searched.contains_key(k) && *v < lowest_cost {
+            lowest_cost = *v;
+            lowest_cost_node = Some(k);
+        }
+    }
+
+    lowest_cost_node
+}
+
+pub fn dijkstra_algorithm<'a>(
+    graph: &HashMap<&'a str, HashMap<&'a str, f64>>,
+    costs: &mut HashMap<&'a str, f64>,
+    parents: &mut HashMap<&'a str, &'a str>,
+    searched: &mut HashMap<&'a str, bool>,
+) {
+    let mut node = match find_lowest_cost_node(costs, searched) {
+        Some(e) => e,
+        None => "none",
+    };
+
+    parents
+        .entry(node)
+        .and_modify(|e| *e = "Start")
+        .or_default();
+
+    while node != "none" {
+        let cost = *costs.get(node).unwrap();
+        let neighbors = graph.get(node).unwrap();
+
+        for (k, v) in neighbors {
+            let new_cost = cost + *v;
+            let old_cost = *costs.get(k).unwrap();
+            if old_cost > new_cost {
+                costs.entry(k).and_modify(|e| *e = new_cost).or_default();
+                parents.entry(k).and_modify(|k| *k = node).or_default();
             }
         }
-    }
 
-    result
-}
-
-pub fn partition(arr: &mut Vec<i32>, low: i32, high: i32) -> i32 {
-    let mut i = low - 1;
-    let pivot = arr[high as usize];
-    for j in low..=high {
-        if arr[j as usize] < pivot {
-            i += 1;
-            arr.swap(j as usize, i as usize);
-        }
-    }
-
-    arr.swap((i + 1) as usize, high as usize);
-
-    (i + 1) as i32
-}
-
-pub fn quick_sort(arr: &mut Vec<i32>, low: i32, high: i32) {
-    if low < high {
-        let index_pivot = partition(arr, low, high);
-        quick_sort(arr, low, index_pivot - 1);
-        quick_sort(arr, index_pivot + 1, high);
+        searched.insert(node, true);
+        node = match find_lowest_cost_node(costs, searched) {
+            Some(e) => e,
+            None => "none",
+        };
     }
 }
 
 fn main() {
-    let mut graph: HashMap<&Person, Vec<&Person>> = HashMap::new();
+    let start_hashmap: HashMap<&str, f64> = HashMap::from([("A", 6.0), ("B", 2.0)]);
+    let a_hashmap: HashMap<&str, f64> = HashMap::from([("Fin", 1.0)]);
+    let b_hashmap: HashMap<&str, f64> = HashMap::from([("A", 3.0), ("Fin", 5.0)]);
 
-    let me = Person {
-        name: String::from("Thong"),
-        value: 10,
-    };
+    let graph: HashMap<&str, HashMap<&str, f64>> = HashMap::from([
+        ("Start", start_hashmap),
+        ("A", a_hashmap),
+        ("B", b_hashmap),
+        ("Fin", HashMap::new()),
+    ]);
 
-    let Bob = Person {
-        name: String::from("Bob"),
-        value: 5,
-    };
+    let mut costs: HashMap<&str, f64> = HashMap::new();
+    let mut parents: HashMap<&str, &str> = HashMap::new();
+    let mut searched: HashMap<&str, bool> = HashMap::new();
 
-    let Claire = Person {
-        name: String::from("Claire"),
-        value: 20,
-    };
-
-    let Alice = Person {
-        name: String::from("Alice"),
-        value: 11,
-    };
-
-    let Peggy = Person {
-        name: String::from("Peggy"),
-        value: 50,
-    };
-
-    let Thom = Person {
-        name: String::from("Thom"),
-        value: 34,
-    };
-
-    let Johny = Person {
-        name: String::from("Johny"),
-        value: 60,
-    };
-
-    let Anuj = Person {
-        name: String::from("Anuj"),
-        value: 58,
-    };
-
-    let me_neighbors = Vec::from([&Bob, &Alice, &Claire]);
-    let alice_neighbors = Vec::from([&Peggy]);
-    let claire_neighbors = Vec::from([&Thom, &Johny]);
-    let bob_neighbors = Vec::from([&Anuj, &Peggy]);
-
-    graph.insert(&me, me_neighbors);
-    graph.insert(&Bob, bob_neighbors);
-    graph.insert(&Claire, claire_neighbors);
-    graph.insert(&Alice, alice_neighbors);
-    graph.insert(&Peggy, Vec::new());
-    graph.insert(&Anuj, Vec::new());
-    graph.insert(&Thom, Vec::new());
-    graph.insert(&Johny, Vec::new());
-
-    dbg!(breadth_first_search(graph, &me));
-
-    let mut temp = Vec::from([3, 4, 10, 5, 2, 9, 7]);
-    let length = temp.len();
-    quick_sort(&mut temp, 0, (length as i32) - 1);
-
-    dbg!(temp);
+    initialize_costs(&mut costs, &graph);
+    initialize_parent(&mut parents, &graph);
+    dijkstra_algorithm(&graph, &mut costs, &mut parents, &mut searched);
 }
